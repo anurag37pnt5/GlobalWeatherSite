939b63b5a5ab0e27b533258cf1aedbd050374940
Anurag Baral <anuragbaral@Anurags-Laptop.local>
2023-04-29 09:48:46 -0400

    Use CORS proxy to fix HTTPS issue

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-const apiKey = '9963cad83c99e758dc3fb007b82781ac';
-const weatherForm = document.getElementById('weather-form');
-const weatherDiv = document.getElementById('weather');
+const API_KEY = "9963cad83c99e758dc3fb007b82781ac";
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
-weatherForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const city = document.getElementById('city').value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const temperature = Math.round(data.main.temp);
-            const description = data.weather[0].description;
-            weatherDiv.innerHTML = `<p>Current weather in ${city}:</p><p>${temperature}°F - ${description}</p>`;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            weatherDiv.innerHTML = `<p>Unable to fetch weather data for ${city}. Please check the city name and try again.</p>`;
-        });
-});
+function getWeatherData(city) {
+  const url = `${CORS_PROXY}http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
+}
